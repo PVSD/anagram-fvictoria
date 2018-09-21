c1250d5509026661e6ae403cea612949ff3db4cc
fvictoria <felipevictoria630@yahoo.com>
2018-09-20 22:14:23 -0400

    hey there

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,17 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	Scanner kbInput  = new Scanner(System.in);
+        Scanner kbInput = new Scanner(System.in);
         System.out.println("Enter first word");
         String S1 = kbInput.nextLine();
         System.out.println("Enter second word");
         String S2 = kbInput.nextLine();
-        
-
-
+        if (S1.length() != S2.length()) {
+            System.out.println("These words are not anagrams");
+        }
+        else
+        {
+            
+        }
 
     }
-}
